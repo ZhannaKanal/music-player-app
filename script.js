599180a0796071d5c3f2cbbaef73da67ff61eb26
Zhanna <kezhanna.03@mail.ru>
2025-10-29 22:03:03 +0500

    tasks 30-33

--- script.js
+++ script.js
@@ -98,5 +98,16 @@ const renderSongs = (array) => {
 };
 renderSongs(userData?.songs);
 const sortSongs = () => {
-  userData?.songs.sort((a, b) => {});
+  userData?.songs.sort((a, b) => {
+    if (a.title < b.title) {
+      return -1;
+    }
+
+    if (a.title > b.title) {
+      return 1;
+    }
+
+    return 0;
+  });
+  return userData?.songs;
 };

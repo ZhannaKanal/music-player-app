cc9d73c07229cf912589e3b4dce7688db1a63f8b
Zhanna <kezhanna.03@mail.ru>
2025-11-13 12:57:53 +0500

    task 90

--- script.js
+++ script.js
@@ -200,6 +200,8 @@ const renderSongs = (array) => {
   if (userData?.songs.length === 0) {
     const resetButton = document.createElement("button");
     const resetText = document.createTextNode("Reset Playlist");
+    resetButton.id = "reset";
+    resetButton.ariaLabel = "Reset playlist";
   }
 };
 
